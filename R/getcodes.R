80146a206fdccd79e0e2a163805fed1b270cf698
vjcitn <stvjc@channing.harvard.edu>
2026-04-10 06:39:14 -0400

    remove private path

--- R/getcodes.R
+++
@@ -1,8 +0,0 @@
-#' helper in channing system
-#' @export
-getcodesNOURISHbase = function() {
- ans = try(haven::read_sas("~stmst00/datasets/basemedusage_all.sas7bdat"))
- if (inherits(ans, "try-error")) stop("only working on stmst00-enabled channing account")
- ans
-}
-
